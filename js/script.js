9ced269a7e22690c1be02aa9e35e94fe8d806844
Samuli Jarvinen <frye@github.com>
2022-01-19 00:51:40 -0800

    Sving data to localStorage

--- js/script.js
+++ js/script.js
@@ -3,13 +3,16 @@ const workDayLastH = 17 //5PM
 
 var blockContainer = $('.container');
 
+var currentDate = moment().format('dddd, MMM Do');
+$('#currentDay').text(currentDate);
+
+    //var now = moment().hour(); //hardcoded below for testing. Swap for real deal.
+    var now = 14;
 
 // Use jQuery to create a time block
 var newTimeBlock = function (blockHour) {
 
-    //var now = moment().hour(); //hardcoded below for testing. Swap for real deal.
-    var now = 14;
-    console.log(now);
+
 
 
     var blockSection = $('<section>');
@@ -22,6 +25,11 @@ var newTimeBlock = function (blockHour) {
     blockTime.text(blockHour + ':00');
     blockTime.addClass('col-2 hour');
 
+    eventText = localStorage.getItem(blockTime.text()) || '';
+    if (eventText) {
+        blockInput.text(eventText);
+    }
+
     blockInput.addClass('col-8');
     if ( blockHour < now ) {
         blockInput.addClass('past');
@@ -30,6 +38,7 @@ var newTimeBlock = function (blockHour) {
     } else {
         blockInput.addClass('future');
     }
+    blockInput.css('color', 'black');
 
     blockButton.text('Save');
     blockButton.addClass('col-2 saveBtn')
@@ -41,6 +50,12 @@ var newTimeBlock = function (blockHour) {
     return blockSection;
 }
 
+var clickHandler = function(event) {
+    localStorage.setItem(event.delegateTarget.children[0].innerText, event.delegateTarget.children[1].value)
+}
+
 for (var i = workDayFisrtH; i<= workDayLastH; i++) {
     blockContainer.append(newTimeBlock(i));
 }
+
+$('section').on('click', 'button', clickHandler);
