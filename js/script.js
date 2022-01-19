278dfcb1bd98a16ef6b506f7d90656eccac5a5e6
Samuli Jarvinen <frye@github.com>
2022-01-19 13:59:03 -0800

    Adding few more comments to the .js

--- js/script.js
+++ js/script.js
@@ -1,20 +1,19 @@
-const workDayFisrtH = 9 //9AM
-const workDayLastH = 17 //5PM
+const workDayFisrtH = 9 // Setting the start of the workday to 9AM
+const workDayLastH = 17 // Setting the last working hour to start at 5PM
 
 var blockContainer = $('.container');
 
+// Format and display the current date in the hero section.
 var currentDate = moment().format('dddd, MMM Do');
 $('#currentDay').text(currentDate);
 
-    //var now = moment().hour(); //hardcoded below for testing. Swap for real deal.
-    var now = 14;
+    var now = moment().hour(); //hardcoded below for testing. Swap for real deal.
+    //var now = 14;
 
 // Use jQuery to create a time block
 var newTimeBlock = function (blockHour) {
 
-
-
-
+    // Set up the elements
     var blockSection = $('<section>');
     var blockTime = $('<time>');
     var blockInput = $('<textarea>');
@@ -22,14 +21,17 @@ var newTimeBlock = function (blockHour) {
 
     blockSection.addClass('row time-block');
 
+    // Set the time field
     blockTime.text(blockHour + ':00');
     blockTime.addClass('col-2 hour');
 
+    // Check localStorage for saved content for the hour in question
     eventText = localStorage.getItem(blockTime.text()) || '';
     if (eventText) {
         blockInput.text(eventText);
     }
 
+    // Create the textarea with potentially earlier saved data.
     blockInput.addClass('col-8');
     if ( blockHour < now ) {
         blockInput.addClass('past');
@@ -40,9 +42,11 @@ var newTimeBlock = function (blockHour) {
     }
     blockInput.css('color', 'black');
 
+    // Save button
     blockButton.text('Save');
     blockButton.addClass('col-2 saveBtn')
 
+    // Add elements to DOM
     blockSection.append(blockTime);
     blockSection.append(blockInput);
     blockSection.append(blockButton);
